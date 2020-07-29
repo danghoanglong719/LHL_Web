039356d58d0af17ebde899dab26d5d7324fc9c7d
HieuTran <63003697+Hieupro789987@users.noreply.github.com>
2020-07-29 17:19:19 +0700

    Update delete.php

--- Layout_Web/PDO/Manager/delete.php
+++ Layout_Web/PDO/Manager/delete.php
@@ -17,3 +17,21 @@
  }
 
 ?>
+                            
+                            <?php
+    include_once("../DataProvider.php");
+    if(isset($_GET['id']))
+    {
+        $user_id = $_GET['id'];
+        
+        $sql = "DELETE FROM `admin` WHERE  `id_admin` = '$user_id'";
+        $result = DataProvider::ExecuteQuery($sql);
+        if($result==true)
+        {
+            header("location:Manager.php");
+        }
+        else{
+            echo "that bai";
+        }
+    }
+?>
